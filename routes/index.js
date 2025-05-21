d528881387719c8f9497246fee5d22205610c043
yairf1 <128710549+yairf1@users.noreply.github.com>
2025-05-21 18:04:05 +0300

    bug fix on routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@
 const {Router} = require('express');
 const router = Router();
 
-const about = require('./about/about.js');
-const home = require('./home/home.js');
-const solution = require('./solution/solution.js');
+const about = require('../about/about.js');
+const home = require('../home/home.js');
+const solution = require('../solution/solution.js');
 
 router.use('/about', about);
 router.use('/', home);
