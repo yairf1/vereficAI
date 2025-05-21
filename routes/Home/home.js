fb713abbb17fa54d3f4d113f75acc950ee8a4c2c
yairf1 <128710549+yairf1@users.noreply.github.com>
2025-05-21 19:23:35 +0300

    bug fix

--- routes/Home/home.js
+++ routes/Home/home.js
@@ -3,7 +3,7 @@ const router = Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '../../../public/homePage/home.html'));
+    res.sendFile(path.join(__dirname + '../../../public/HomePage/home.html'));
 });
 
 module.exports = router;
