6e531aeb57fc9600a534491237bed3dbf10b10a3
ShaiF29 <furman.shai@gmail.com>
2025-06-11 19:41:45 +0300

    team page add

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-
 const {Router} = require('express');
 const router = Router();
 
 const about = require('../routes/about/about.js');
 const home = require('../routes/Home/home.js');
 const solution = require('../routes/solution/solution.js');
+const team = require('../routes/team/team.js');
 
 router.use('/about', about);
 router.use('/', home);
 router.use('/solution', solution);
+router.use('/team', team);
 
 module.exports = router;
