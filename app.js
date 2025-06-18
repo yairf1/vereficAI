b3d624107190c44c351728c8967b7d26b424641f
odeyap <odeyap08@gmail.com>
2025-06-18 17:52:41 +0300

    changed comments from Hebrew to English.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 
-//  הפעלת השרת
+// Starting the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
